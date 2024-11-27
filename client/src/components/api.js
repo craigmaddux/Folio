0cff4de52596a558415888ffc2a9caadc4986728
craigmaddux <craig.maddux@gmail.com>
2024-11-27 22:11:55 +0000

    url debug

--- client/src/components/api.js
+++ client/src/components/api.js
@@ -1,6 +1,7 @@
 const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://folio-server-app-ftg0eecmb5dcg9eb.eastus2-01.azurewebsites.net:8-80/api';
 
 export const fetchFromAPI = async (endpoint, options = {}) => {
+    console.log("Checking url...");
     console.log(`${API_BASE_URL}${endpoint}`);
   const response = await fetch(`${API_BASE_URL}${endpoint}`, {
     headers: {
