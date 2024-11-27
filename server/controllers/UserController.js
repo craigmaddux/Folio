0bd03a2ab1c0af1de852bc043b465c4e50d519e9
Craig Maddux <craig.maddux@gmail.com>
2024-11-26 20:06:04 -0600

    bcrypt update

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,5 @@
 // server/controllers/UserController.js
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
 const db = require('../db'); 
 
