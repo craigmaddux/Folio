5a52c115d3ddb2f705f081678b007e0a94027fd9
craigmaddux <craig.maddux@gmail.com>
2024-11-28 16:13:45 +0000

    login update to api wrapper

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 import './Login.css';
+import { fetchFromAPI } from './api';
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -14,7 +15,7 @@ const Login = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('/api/login', {
+      const response = await fetchFromAPI('/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
