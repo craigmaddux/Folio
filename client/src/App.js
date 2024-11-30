e8636957d70beb027e665bae2df9b282d5361358
craigmaddux <craig.maddux@gmail.com>
2024-11-30 17:21:42 +0000

    route logging

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import PurchaseCreditsPage from './components/PurchaseCreditsPage';
 import Checkout from './components/CheckoutPage';
 import PaymentSuccessPage from './components/PaymentSuccessPage'; 
 import MyAccount from './components/MyAccount';
-
+import RouteLogger from './RouteLogger';
 
 
 function AppContent() {
@@ -98,6 +98,7 @@ function App() {
   return (
     <Router>
       <AuthProvider>
+        <RouteLogger />
           <AppContent />
       </AuthProvider>
     </Router>
---
+++ client/src/components/RouteLogger.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const RouteLogger = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(`Navigated to route: ${location.pathname}`);
+    console.log(`Full URL: ${location.pathname}${location.search}`);
+  }, [location]);
+
+  return null; // This component doesn't render anything
+};
+
+export default RouteLogger;
