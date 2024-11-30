168bf50951fdd11d3eefa58109f43c8d26b8349b
craigmaddux <craig.maddux@gmail.com>
2024-11-30 16:04:07 +0000

    cors debugging

--- server/index.js
+++ server/index.js
@@ -18,9 +18,11 @@ const allowedOrigins = [
 app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (e.g., mobile apps, curl requests)
+    
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      console.log("Origin: " + origin);
+      const msg = 'The CORS policy for this site does not allow access from the specified origin: ';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
