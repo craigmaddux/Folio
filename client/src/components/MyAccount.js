d7c3773caadf6e40c1ebe5ddcf144f04337d5bb1
craigmaddux <craig.maddux@gmail.com>
2024-12-03 18:49:47 +0000

    myaccount css bugfix

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchFromAPI } from './api';
-import './AccountPage.css';
+import './MyAccount.css';
 
 const AccountPage = () => {
   const [user, setUser] = useState(null);
