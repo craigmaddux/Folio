4b92d18ef45d39ca65e1931e94da3f694f84f20c
craigmaddux <craig.maddux@gmail.com>
2024-12-03 16:51:08 +0000

    nodemailer removal

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,4 +1,4 @@
-const nodemailer = require('nodemailer');
+
 const axios = require('axios');
 // Logic App URL
 const LOGIC_APP_URL = 'https://prod-18.eastus2.logic.azure.com:443/workflows/efd664f8eced40a9b375c3a65559197a/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=VltLAaPCHjN8zgBXYXvitRLri5uAConzmBzwMfYoXI4';
